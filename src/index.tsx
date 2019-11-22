ec4fc81e7f975c595bd28b9ea449dd870a94ca01
austonpramodh <austonpramodh@gmail.com>
2019-11-22 15:23:16 +0530

    Async APP componnet loading added back for better performance

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
-import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const App = React.lazy(() => import("./App"));
+
+ReactDOM.render(
+    <React.Suspense fallback={<span></span>}>
+        <App />
+    </React.Suspense>,
+    document.getElementById("root"),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
