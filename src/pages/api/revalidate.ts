88c1e31cdaea4c25bf5a7011ed6b0dff0dc5b539
Auston Pramodh Barboza <austonpramodh@gmail.com>
2023-03-01 14:35:12 -0500

    chore: test trigger ack

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -15,6 +15,11 @@ import { linkResolver, createClient } from "../../prismicio";
  * The Prismic webhook must send the correct secret.
  */
 const handler: NextApiHandler = async (req, res) => {
+    //  Test trigger
+    if (req.body.type === "test-trigger") {
+        return res.json({ message: "Test trigger received" });
+    }
+
     if (req.body.type === "api-update" && req.body.documents.length > 0) {
         // Check for secret to confirm this is a valid request
         if (req.body.secret !== process.env.PRISMIC_WEBHOOK_SECRET) {
