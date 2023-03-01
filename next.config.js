8345e171f3b69ae446b729ff1877311b0d04d5b7
Auston Pramodh Barboza <austonpramodh@gmail.com>
2023-02-28 20:00:42 -0500

    chore: added resume.auston.dev

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "https://resume.auston.dev",
+        destination: "/resume",
+        permanent: false,
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["ipv6-test.com"],
