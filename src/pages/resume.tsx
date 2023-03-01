8bdab94fbad3e2bf69db1e4e1eaeb2a796d9d6ac
Auston Pramodh Barboza <austonpramodh@gmail.com>
2023-02-28 20:37:45 -0500

    fix: linting issues

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -4,73 +4,68 @@ import { createClient } from "../prismicio";
 import { NextPage, GetStaticProps } from "next";
 import { Content } from "@prismicio/client";
 import Layout from "../components/PageLayout";
-import StaticDataContext, {
-  StaticDataContextType,
-} from "../components/StaticDataContext";
+import StaticDataContext, { StaticDataContextType } from "../components/StaticDataContext";
 import SEO from "../components/Seo";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 type PageProps = {
-  staticDataContext: StaticDataContextType;
+    staticDataContext: StaticDataContextType;
 };
 
 const Resume: NextPage<PageProps> = ({ staticDataContext }) => {
-  const link = staticDataContext.resumeData?.resume_doc
-    ? prismicH.asLink(staticDataContext.resumeData?.resume_doc)
-    : "";
+    const link = staticDataContext.resumeData?.resume_doc
+        ? prismicH.asLink(staticDataContext.resumeData?.resume_doc)
+        : "";
 
-  const router = useRouter();
+    const router = useRouter();
 
-  useEffect(() => {
-    setTimeout(() => {
-      if (link) {
-        router.push(link);
-      }
-    }, 500);
-  }, [link, router]);
+    useEffect(() => {
+        setTimeout(() => {
+            if (link) {
+                router.push(link);
+            }
+        }, 500);
+    }, [link, router]);
 
-  return (
-    <StaticDataContext.Provider value={staticDataContext}>
-      <Layout>
-        <SEO />
-        Redirecting to resume...
-        {/* TODO: add some loading animation */}
-      </Layout>
-    </StaticDataContext.Provider>
-  );
+    return (
+        <StaticDataContext.Provider value={staticDataContext}>
+            <Layout>
+                <SEO />
+                Redirecting to resume...
+                {/* TODO: add some loading animation */}
+            </Layout>
+        </StaticDataContext.Provider>
+    );
 };
 
 export default Resume;
 
-export const getStaticProps: GetStaticProps<PageProps> = async ({
-  params,
-  previewData,
-}) => {
-  const client = createClient({ previewData });
+export const getStaticProps: GetStaticProps<PageProps> = async ({ params, previewData }) => {
+    const client = createClient({ previewData });
 
-  const uid = params?.pagePath?.[params.pagePath.length - 1] || "home";
-  const page = await client.getByUID("page", uid);
+    const uid = params?.pagePath?.[params.pagePath.length - 1] || "home";
+    const page = await client.getByUID("page", uid);
 
-  let seoDataDoc: Content.SeoDataDocument | null = null;
-  let resumeDoc: Content.ResumeDocument | null = null;
+    let seoDataDoc: Content.SeoDataDocument | null = null;
+    let resumeDoc: Content.ResumeDocument | null = null;
 
-  try {
-    seoDataDoc = await client.getSingle("seo_data");
-    resumeDoc = await client.getSingle("resume");
-  } catch (error) {
-    // No external links document found
-    console.log("error", error);
-  }
+    try {
+        seoDataDoc = await client.getSingle("seo_data");
+        resumeDoc = await client.getSingle("resume");
+    } catch (error) {
+        // No external links document found
+        console.log("error", error);
+    }
 
-  return {
-    props: {
-      page,
-      staticDataContext: {
-        externalLinksData: null,
-        seoData: seoDataDoc?.data || null,
-        resumeData: resumeDoc?.data || null,
-      },
-    },
-  };
+    return {
+        props: {
+            page,
+            staticDataContext: {
+                externalLinksData: null,
+                seoData: seoDataDoc?.data || null,
+                resumeData: resumeDoc?.data || null,
+            },
+        },
+    };
 };
