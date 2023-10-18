f19868230e16270de176feaa070a50a7b4a794ab
Auston Barboza <austonpramodh@gmail.com>
2023-10-18 11:39:44 -0500

    chore: linting fixes

--- src/components/StaticDataContext/index.tsx
+++ src/components/StaticDataContext/index.tsx
@@ -2,9 +2,9 @@ import { Content } from "@prismicio/client";
 import React from "react";
 
 export type StaticDataContextType = {
-  externalLinksData?: Content.ExternalLinksDocumentData | null;
-  seoData?: Content.SeoDataDocumentData | null;
-  resumeData?: Content.ResumeDocumentData | null;
+    externalLinksData?: Content.ExternalLinksDocumentData | null;
+    seoData?: Content.SeoDataDocumentData | null;
+    resumeData?: Content.ResumeDocumentData | null;
 };
 
 const StaticDataContext = React.createContext<StaticDataContextType>({});
