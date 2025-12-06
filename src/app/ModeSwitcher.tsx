b5cf7004f20e02ef498815e7b8d1f48afd505156
Auston Barboza <austonpramodh@gmail.com>
2025-12-06 11:55:58 -0600

    chore: add system mode for mode switcher

--- src/app/ModeSwitcher.tsx
+++ src/app/ModeSwitcher.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { DarkMode, LightMode } from "@mui/icons-material";
+import { BrightnessAuto, DarkMode, LightMode } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 import { useColorScheme } from "@mui/material/styles";
 
 export const ModeSwitcher = () => {
     const { mode, setMode } = useColorScheme();
+    console.log(mode);
+
+    const cycleMode = () => {
+        if (mode === "light") setMode("dark");
+        else if (mode === "dark") setMode("system");
+        else setMode("light"); // system → light
+    };
+
     return (
         <>
             <IconButton
-                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                onClick={cycleMode}
                 sx={{
                     position: "fixed",
                     top: 12,
@@ -29,6 +37,10 @@ export const ModeSwitcher = () => {
                 />
                 <LightMode
                     sx={{ display: mode === "light" ? "block" : "none" }}
+                />
+
+                <BrightnessAuto
+                    sx={{ display: mode === "system" ? "block" : "none" }}
                 />
             </IconButton>
         </>
